41a240d51a858f413c02eaccfa52d8400a59e6c8
Beyza Nur Deniz <beyza.dana@ogr.sakarya.edu.tr>
2021-12-18 23:27:29 +0300

    add_commit

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -15,6 +15,8 @@ namespace WebProje.Controllers
         {
             Context c = new Context();
             ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2 = c.Blogs.Where(x=>x.WriterID==1).Count();
+            ViewBag.v3 = c.Categories.Count();
             return View();
         }
     }
