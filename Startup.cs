828dfd902fee5b2d17b1ef803d84417d8ea7eb89
Yusuf-Karakaya <yusuf.karakaya1@ogr.sakarya.edu.tr>
2021-12-25 03:24:56 +0300

    add-areas

--- Startup.cs
+++ Startup.cs
@@ -70,10 +70,16 @@ namespace WebProje
 
             app.UseEndpoints(endpoints =>
             {
+                 endpoints.MapControllerRoute(
+                 name: "areas",
+                 pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+               );
+
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+          
         }
     }
 }
